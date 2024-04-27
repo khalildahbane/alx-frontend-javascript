cbf7ce3a94856d350d3d4d307034279f50460143
khalildahbane <kelldani100@gmail.com>
2024-04-27 15:54:51 +0000

    ES6 Promises

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
 // A stand user's promise
 export default function signUpUser(firstName, lastName) {
-    return prompt.resolve({ firstName, lastName });
+  return Promise.resolve({ firstName, lastName });
 }
