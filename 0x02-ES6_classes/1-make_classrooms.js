e8bffa697e859c2c07175cd8315bf124f7f70d74
khalildahbane <kelldani100@gmail.com>
2024-04-28 13:25:29 +0000

    ES6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const sizes = [19, 20, 34];
-    return sizes.map(size => new ClassRoom(size));
- }
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
