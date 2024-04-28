da8b8fd3ac0b3814fab2590c79ea1ad4b93cb6ee
khalildahbane <kelldani100@gmail.com>
2024-04-28 13:10:44 +0000

    ES6 classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
-// add
+// at some point i attended this
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
