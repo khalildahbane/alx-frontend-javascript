b9f5aa871d55cfa2d7cb3b8721b23be0eca9c26e
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-04-30 14:36:12 +0100

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,9 @@
 //  returns an array of objects.
 function getListStudents() {
- 
+    const students = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' }
+        { id: 2, firstName: 'James', location: 'Columbia' }
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
+    return students;
 }
