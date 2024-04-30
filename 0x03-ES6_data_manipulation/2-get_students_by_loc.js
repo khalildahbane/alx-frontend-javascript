bdc318f5aad231e06a3a0b97f506d5165d93ead0
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-04-30 15:57:05 +0100

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,6 @@
 // Getting the students by the location
 function getStudentsByLocation(students, city) {
- if (!Array.isArray(students)) 
-  return [];
+  if (!Array.isArray(students)) return [];
 
   return students.filter(student => student.location === city);
 }
