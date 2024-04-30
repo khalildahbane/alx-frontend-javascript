64869299aa45f7062f7d1f6e4e2cf2a698008c70
khalildahbane <kelldani100@gmail.com>
2024-04-30 14:27:56 +0000

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
-// returns an array of ids from a list of object
+// students ids
 function getListStudentIds(students) {
  if (!Array.isArray(students)) 
   return [];
- 
  return students.map(student => student.id);
 }
 export default getListStudentIds;
